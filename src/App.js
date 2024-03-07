a7e055ffb7d35936d54dbda6116f9f3759034554
irisahah <irisharjo@gmail.com>
2024-03-07 19:37:48 +0200

    v

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import '/linnad.css';
 import Linn from './Linn';
-import londonPilt from '';
+import londonPilt from './pildid/london.jpg';
+import pariisPilt from './pildid/pariis.jpg';
+
+const linnad = [
+  pariis,
+  {nimi: 'London, riik: 'Inglismaa', pilt: londonPilt};
+
+]
+
 
 function App() {
   return (
     <div className="container">
       <h1>Minu lemmiklinnad</h1>
       <Linn name="Pariis" riik="Prantsusmaa" />
-      <Linn name="London" riik="INglismaa" />
+      <Linn name="London" riik="Inglismaa" />
       <Linn name="Elva" riik="Eesti" />
     </div>
   )
 }
+
 
 export default App;
